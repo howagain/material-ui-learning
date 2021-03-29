652b39e274e61cfba29e92c5cfa7355ae6b3ddb6
Jacob Haugen <jh@Jacobs-MacBook-Air.local>
2021-03-29 13:25:56 -0400

    027 Active Tab refresh fix

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,14 +1,14 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import useScrollTrigger from "@material-ui/core/useScrollTrigger";
-import { makeStyles } from '@material-ui/core/styles';
-import Tabs from "@material-ui/core/Tabs"
-import Tab from "@material-ui/core/Tab"
-import Button from "@material-ui/core/Button"
-import {Link} from "react-router-dom"
+import { makeStyles } from "@material-ui/core/styles";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Button from "@material-ui/core/Button";
+import { Link } from "react-router-dom";
 
-import logo from "../../assets/logo.svg"
+import logo from "../../assets/logo.svg";
 
 function ElevationScroll(props) {
   const { children } = props;
@@ -27,63 +27,124 @@ function ElevationScroll(props) {
   });
 }
 
-const useStyles = makeStyles(theme=>({
+const useStyles = makeStyles((theme) => ({
   toolbarMargin: {
     ...theme.mixins.toolbar,
-    marginBottom: "3em"
+    marginBottom: "3em",
   },
-  logo:{
-    height: '7em'
+  logo: {
+    height: "7em",
   },
-  tabContainer:{
-    marginLeft: "auto"
+  logoContainer:{
+    padding: 0,
+    // stop transparent overlay
+    "&:hover": {
+      backgroundColor: "transparent"
+    }
   },
-  tab:{
+  tabContainer: {
+    marginLeft: "auto",
+  },
+  tab: {
     ...theme.typography.tab,
     minWidth: 10,
-    marginLeft: "25px"
+    marginLeft: "25px",
   },
-  button:{
+  button: {
     ...theme.typography.estimate,
     borderRadius: "50px",
     marginRight: "25px",
     marginLeft: "50px",
-  }
-}))
+  },
+}));
 
 const Header = () => {
-  const classes = useStyles()
+  const classes = useStyles();
 
-  const [value, setValue] = useState(0)
+  const [value, setValue] = useState(0);
 
   const handleChange = (e, value) => {
-    setValue(value)
-  }
+    setValue(value);
+  };
+
+  useEffect(() => {
+    if (window.location.pathname === "/" && value !== 0) {
+      setValue(0);
+    } else if (window.location.pathname === "/services" && value !== 1) {
+      setValue(1);
+    } else if (window.location.pathname === "/revolution" && value !== 2) {
+      setValue(2);
+    } else if (window.location.pathname === "/about" && value !== 3) {
+      setValue(3);
+    } else if (window.location.pathname === "/contact" && value !== 4) {
+      setValue(4);
+    } else if (window.location.pathname === "/estimate" && value !== 5) {
+      setValue(5);
+    }
+  }, [value]);
 
   return (
     <React.Fragment>
-    {/* This creates a subtle scroll effect on the page when you scroll down, slighlty elivatingit and giving it a drop shadow. */}
-    <ElevationScroll>
-      {/* The app bar has a default position of fixed, which fixes the app bar to the top of the page regardless of scroll position
+      {/* This creates a subtle scroll effect on the page when you scroll down, slighlty elivatingit and giving it a drop shadow. */}
+      <ElevationScroll>
+        {/* The app bar has a default position of fixed, which fixes the app bar to the top of the page regardless of scroll position
     Static keeps it at the top when you scroll so it disappears 
     Color comes from Theme
     */}
-      <AppBar position="fixed">
-        {/* Why do we need the toolbar to allow horizontal rather than vertical layout*/}
-        <Toolbar disableGutters>
-          <img alt="company logo" className={classes.logo} src={logo}/>
-          <Tabs value={value} onChange={handleChange} className={classes.tabContainer} indicatorColor="primary">
-            <Tab className={classes.tab} component={Link} to="/" label="Home"/>
-            <Tab className={classes.tab} component={Link} to="/services" label="Services"/>
-            <Tab className={classes.tab} component={Link} to="/revolution" label="The Revolution"/>
-            <Tab className={classes.tab} component={Link} to="/about" label="About Us"/>
-            <Tab className={classes.tab} component={Link} to="/contact" label="Contact Us"/>
-          </Tabs>
-          <Button variant="contained" color="secondary" className={classes.button}>Free Estimate</Button>
-        </Toolbar>
-      </AppBar>
-    </ElevationScroll>
-    <div className={classes.toolbarMargin} />
+        <AppBar position="fixed">
+          {/* Why do we need the toolbar to allow horizontal rather than vertical layout*/}
+          <Toolbar disableGutters>
+            <Button component={Link} to="/" className={classes.logoContainer} onClick={()=> setValue(0)} disableRipple>
+              <img alt="company logo" className={classes.logo} src={logo} />
+            </Button>
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              className={classes.tabContainer}
+              indicatorColor="primary"
+            >
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/"
+                label="Home"
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/services"
+                label="Services"
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/revolution"
+                label="The Revolution"
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/about"
+                label="About Us"
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to="/contact"
+                label="Contact Us"
+              />
+            </Tabs>
+            <Button
+              variant="contained"
+              color="secondary"
+              className={classes.button}
+            >
+              Free Estimate
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
+      <div className={classes.toolbarMargin} />
     </React.Fragment>
   );
 };
