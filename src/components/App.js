1316e1d7e3d983e683fd2d1f09a5f33df57504ca
Jacob Haugen <jh@Jacobs-MacBook-Air.local>
2021-03-29 13:12:37 -0400

    react router added

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import {ThemeProvider} from "@material-ui/styles"
+import {BrowserRouter,Route,Switch} from "react-router-dom"
 
 import theme from './ui/Theme'
 import Header from '../components/ui/Header'
@@ -6,8 +7,22 @@ import Header from '../components/ui/Header'
 function App() {
   return (
     <ThemeProvider theme={theme} >
+      <BrowserRouter>
       <Header />
-      Hello
+      <Switch>
+        <Route exact path="/" component={()=><div>Home</div>}/>
+        <Route exact path="/services" component={()=><div>services</div>}/>
+        <Route exact path="/customsoftware" component={()=><div>customsoftware</div>}/>
+        <Route exact path="/mobileapps" component={()=><div>mobileapps</div>}/>
+        <Route exact path="/websites" component={()=><div>websites</div>}/>
+        <Route exact path="/revolution" component={()=><div>revolution</div>}/>
+        <Route exact path="/about" component={()=><div>about</div>}/>
+        <Route exact path="/contact" component={()=><div>contact</div>}/>
+        <Route exact path="/estimate" component={()=><div>estimate</div>}/>
+      </Switch>
+
+      </BrowserRouter>
+      
     </ThemeProvider>
   );
 }
